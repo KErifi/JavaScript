a854dd1d3008b7dea1e188ae1950f8fbfabbc00a
KErifi <krisnina036426@gmail.com>
2026-03-11 18:10:48 +0300

    ErrorHandling

--- ErrorHandling/script.js
+++ ErrorHandling/script.js
@@ -7,6 +7,7 @@ function safeDivide(a, b) {
         return a / b;
     } catch (error) {
         console.error(error.message);
+        return undefined;
     }
 }
 
@@ -14,17 +15,18 @@ console.log(safeDivide(10, 2));
 console.log(safeDivide(10, 0));
 
 // 2
-function transfromJSON(jsonString) {
+function transformJSON(jsonString) {
     try {
         const obj = JSON.parse(jsonString);
         return obj;
     } catch (error) {
         console.error("Ошибка при парсинге JSON:", error.message);
+        return undefined;
     }
 }
 
-console.log(transfromJSON('{"name":"Кристина","age":22}'));
-console.log(transfromJSON('invalid JSON'));
+console.log(transformJSON('{"name":"Кристина","age":28}')); 
+console.log(transformJSON('invalid JSON'));
 
 // 3
 function checkAccess(age) {
